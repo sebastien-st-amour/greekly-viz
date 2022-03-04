e19b1de76b2d05a8e928b87d4eb463ab41130682
Sebastien St-Amour <sebastien@Sebastiens-MacBook-Pro.local>
2022-03-04 16:49:34 -0500

    refactor fetchStocks into services/greekly

--- index.js
+++ index.js
@@ -1,48 +1,19 @@
 const express = require('express');
 const path = require('path');
-const axios = require('axios');
 
 const app = express();
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
 
 dotenv.config()
 
-// Serve static files from the React app
+const greekly = require('./services/greekly');
+
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 
+app.get('/api/stocks', greekly.fetchStocks);
 
-// Put all API endpoints under '/api'
-app.get('/api/stocks', async (req, res) => {
 
-  console.log('api url: ', process.env.GREEKLY_API_URL);
-
-  try {
-
-    const token = await axios.post(`${process.env.GREEKLY_API_URL}/api/login`, {
-      email: process.env.GREEKLY_API_USER,
-      password: process.env.GREEKLY_API_PASSWORD
-    });
-  
-    console.log('token: ', token.data.access_token);
-  
-    const data = await axios.get(`${process.env.GREEKLY_API_URL}/api/stocks`, {
-      headers: {
-        Authorization: `Bearer ${token.data.access_token}`
-      }
-    })
-
-    console.log('data: ', data.data);
-
-    res.json(data.data);
-
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
---
+++ services/greekly.js
@@ -0,0 +1,87 @@
+
+
+const axios = require('axios');
+const NodeCache = require('node-cache');
+const url = require('url');
+
+
+const tokenCache = new NodeCache({ stdTTL: 60 * 60 * 24 });
+
+const fetchToken = async () => {
+  
+  try {
+    
+    const token = await axios.post(`${process.env.GREEKLY_API_URL}/api/login`, {
+      email: process.env.GREEKLY_API_USER,
+      password: process.env.GREEKLY_API_PASSWORD
+    });
+
+    tokenCache.set('token', token.data.access_token, 60 * 60 * 24);
+    
+    return token.data.access_token;
+  
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const fetchStocks = async (req, res) => {
+
+  try {
+    
+    const token = await tokenCache.get('token') || await fetchToken();
+
+    const data = await axios.get(`${process.env.GREEKLY_API_URL}/api/stocks`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+
+    res.json(data.data);
+  
+  } catch (error) {
+
+    if (error.response.status === 401) {
+      
+      const token = await fetchToken();
+      
+      return await fetchStocks();
+    
+    }
+    console.log(error);
+  }
+}
+
+const fetchOptionContracts = async (req, res) => {
+
+
+  try {
+
+    const token = await tokenCache.get('token') || await fetchToken();
+
+    const data = await axios.get(`${process.env.GREEKLY_API_URL}/api/option_contracts`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+
+    res.json(data.data);
+
+  } catch (error) {
+
+    if (error.response.status === 401) {
+
+      const token = await fetchToken();
+
+      return await fetchOptionContracts();
+
+    }
+    console.log(error);
+  }
+}
+
+module.exports = {
+  fetchToken,
+  fetchStocks,
+  fetchOptionContracts
+}
